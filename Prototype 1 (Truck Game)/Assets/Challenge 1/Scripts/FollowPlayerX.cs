5b3b066655bb2e0872c2e9b8348e00e73e8ce5f5
AidenHCode <24hadda@elida.k12.oh.us>
2024-02-16 09:16:22 -0500

    Update

--- Prototype 1 (Truck Game)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prototype 1 (Truck Game)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Vector3 offset = new Vector3(15.0f,2.0f,0.0f);
 
     // Start is called before the first frame update
     void Start()
