6a9b21da57d3e9dc157833e50d4c96d21130aeec
AidenHCode <24hadda@elida.k12.oh.us>
2024-03-04 09:16:01 -0500

    Problems

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -10,18 +10,26 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            //Randomly generate animal index and spawn postion
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-                0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
         }
     }
+
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), spawnPos,
+               animalPrefabs[animalIndex].transform.rotation);
+    }
 }
+    
+
