f6977c9b5bd0e420063b879674c3d9985343b20f
AidenHCode <24hadda@elida.k12.oh.us>
2024-03-27 08:45:35 -0400

    New

--- Prototype 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Prototype 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
-    public GameObject[] objectPrefabs;
+    public GameObject objectPrefabs;
     private float spawnDelay = 2;
-    private float spawnInterval = 1.5f;
-
+    private float spawnInterval = 1.5f; 
     private PlayerControllerX playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObject", spawnDelay, spawnInterval);
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
     // Spawn obstacles
-    void SpawnObjects ()
+    void SpawnObject()
     {
         // Set random spawn location and random object index
         Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
-        int index = Random.Range(0, objectPrefabs.Length);
+        int index = Random.Range(objectPrefabs.Length);
 
         // If game is still active, spawn new object
-        if (!playerControllerScript.gameOver)
+        if (!playerControllerScript.gameOver == false)
         {
-            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+            Instantiate(objectPrefabs, spawnLocation, objectPrefabs.transform.rotation);
         }
 
     }
